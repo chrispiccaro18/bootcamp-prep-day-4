5315d66044c9541ba39a1721f5a15cb3846b0d96
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-17 19:45:55 -0800

    Making greeting function

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 // eslint-disable-next-line
 function greet() {
+    
     // get the inputs by id
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
     
     // get the values out of the inputs
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
 
+    
     // make the greeting
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
+    console.log(greeting);
 
     // display the greeting
 
